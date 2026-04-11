ce4eacb3ef58f34311be51454ad6296a2f937b50
Shantanu Chauhan <shantanuchauhan227@gmail.com>
2026-04-11 21:32:19 +0530

    working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.PORT) || 5000
+    }
   }
 })
